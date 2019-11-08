6270040e286037d7d30f198c92b6f0fb3cd4f557
BazuMax <bazuka5801@mail.ru>
2019-11-08 23:43:14 +0300

    feat[server]: init BaseServer

--- src/FastestTimeUDP.Server/AppCore.cs
+++ src/FastestTimeUDP.Server/AppCore.cs
@@ -7,8 +7,12 @@ namespace FastestTimeUDP.Server
     public class AppCore : SapphireType
     {
         private BaseServer _Server;
-        
-        
-        
+
+
+        public override void OnAwake()
+        {
+            _Server = this.AddType<BaseServer>();
+            _Server.Host(22540);
+        }
     }
 }
