057d9b568b2cca31cd3aeb74e1cbeaca189d44da
BazuMax <bazuka5801@mail.ru>
2019-11-08 23:45:58 +0300

    feat[client]: init BaseClient

--- src/FastestTimeUDP.Client/AppCore.cs
+++ src/FastestTimeUDP.Client/AppCore.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 using FastestTimeUDP.Client.Forms;
+using FastestTimeUDP.Common.Network;
 using SapphireEngine;
 
 namespace FastestTimeUDP.Client
 {
     public class AppCore : SapphireType
     {
+        private BaseClient _Client;
+        
+        
         public override void OnAwake()
         {
             base.OnAwake();
 
             StartThreadUI();
+            
+            _Client = this.AddType<BaseClient>();
+            _Client.Connect("127.0.0.1", 22540);
         }
 
         void StartThreadUI()
