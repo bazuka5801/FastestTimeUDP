89b87b26cf15436ab5daea076fa2e17bf0a3b9c0
BazuMax <bazuka5801@mail.ru>
2019-11-09 12:16:48 +0300

    feat[common]: improve client

--- src/FastestTimeUDP.Common/Network/BaseClient.cs
+++ src/FastestTimeUDP.Common/Network/BaseClient.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using SapphireEngine;
-using SapphireEngine.Functions;
+using Timer = SapphireEngine.Functions.Timer;
 
 namespace FastestTimeUDP.Common.Network
 {
+    public class ClientPPSEventArgs : EventArgs
+    {
+        /// <summary>
+        ///   Packets Per Second
+        /// </summary>
+        public int PPS;
+
+        public ClientPPSEventArgs(int pps)
+        {
+            this.PPS = pps;
+        }
+    }
+
+    public delegate void ClientPPSHandler(object sender, ClientPPSEventArgs e);
+    
     public class BaseClient : NetworkPeer
     {
         private int receivedPacketsPerSecond = 0;
         private DateTime lastPacketsUpdate = DateTime.Now;
+        private Timer reconnectTimer;
+
+        private Timer sendAliveTimer;
+
+        public ClientPPSHandler ClientPPS;
         
         public void Connect(string ip, int port)
         {
@@ -18,19 +39,56 @@ namespace FastestTimeUDP.Common.Network
                 throw new Exception($"{nameof(_Net)} is not null!");
             }
             
-            InitPeer(new UdpClient(ip, port));
+            this.IP   = ip;
+            this.Port = port;
+            
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Connect(new IPEndPoint(IPAddress.Parse(ip), port));
+            InitPeer(socket);
+            if (Status != StatusE.Reconnecting)
+            {
+                Status = StatusE.Reconnecting;
+            }
 
-            Timer.SetInterval(SendAlive, 1f);
+            sendAliveTimer = Timer.SetInterval(SendAlive, 1f);
         }
 
-        public void Disconnect()
+        public void Reconnect()
         {
-            base.Dispose();
+            Disconnect();
+            Connect(IP, Port);
+            Timer.SetTimeout(() =>
+            {
+                if (IsConnected() == false) Reconnect();
+            }, 5f);
         }
 
-        protected override void OnNetworkData(byte[] data, IPEndPoint senderIP)
+        public override void Disconnect()
         {
-            var receivedDate = DateTime.FromBinary(BitConverter.ToInt64(data));
+            if (sendAliveTimer != null)
+            {
+                // Destroy timer
+                sendAliveTimer.Enable   = false;
+                sendAliveTimer.Callback = null;
+                sendAliveTimer          = null;
+            }
+
+            base.Disconnect();
+
+            Status = StatusE.Disconnected;
+        }
+    
+
+        protected override void OnNetworkData(int length, EndPoint senderIP)
+        {
+            base.OnNetworkData(length, senderIP);
+            
+            if (Status != StatusE.Connected)
+            {
+                Status = StatusE.Connected;
+            }
+            
+            var receivedDate = DateTime.FromBinary(BitConverter.ToInt64(_Data[..length]));
             
             receivedPacketsPerSecond++;
         }
@@ -38,12 +96,29 @@ namespace FastestTimeUDP.Common.Network
         public override void OnUpdate()
         {
             base.OnUpdate();
+            
+            
+            if (IsConnected() == false)
+            {
+                return;
+            }
 
+            if (DateTime.Now.Subtract(LastReceiveActivityTime).TotalSeconds > 1)
+            {
+                if (reconnectTimer == null && Status !=  StatusE.Reconnecting)
+                {
+                    Status         = StatusE.Reconnecting;
+                    reconnectTimer = Timer.SetTimeout(Reconnect, 5f);
+                }
+
+                return;
+            }
+            
             if (DateTime.Now.Subtract(lastPacketsUpdate).TotalSeconds >= 1)
             {
                 lastPacketsUpdate = DateTime.Now;
                 
-                ConsoleSystem.Log($"Packets per second: {receivedPacketsPerSecond}");
+                ClientPPS?.Invoke(this, new ClientPPSEventArgs(receivedPacketsPerSecond));
                 receivedPacketsPerSecond = 0;
             }
         }
@@ -52,7 +127,7 @@ namespace FastestTimeUDP.Common.Network
         private byte[] alivePacket = new byte[] {255};
         public void SendAlive()
         {
-            _Net.Send(alivePacket, 1);
+            _Net.Send(alivePacket);
         }
     }
 }
