a604770b8d837bb80fdeb064a21949580d6455a6
BazuMax <bazuka5801@mail.ru>
2019-11-09 12:10:13 +0300

    chore[common]: add ex method to run code in ui thread

---
+++ src/FastestTimeUDP.Common/Extensions/Form.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FastestTimeUDP.Common.Extensions
+{
+    public static class FormEx
+    {
+        public static void RunInUI(this Form form, Action action)
+        {
+            if (form.InvokeRequired)
+            {
+                form.Invoke((Action) (() => RunInUI(form, action)));
+                return;
+            }
+            
+            action.Invoke();
+        }
+    }
+}
